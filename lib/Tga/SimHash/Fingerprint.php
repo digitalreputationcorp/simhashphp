72baacc8d0fbe4f2526bed578c45e32c9bbb1db8
simran <simran.azer@mail.ru>
2022-12-12 13:01:12 +0600

    fix Fingerprint getBinary

--- lib/Tga/SimHash/Fingerprint.php
+++ lib/Tga/SimHash/Fingerprint.php
@@ -72,7 +72,7 @@ class Fingerprint
      */
     public function getBinary()
     {
-        return str_pad(decbin($this->decimalValue), $this->size, '0', STR_PAD_LEFT);
+	    return str_pad(decbin((int)$this->decimalValue), $this->size, '0', STR_PAD_LEFT);
     }
 
     /**
